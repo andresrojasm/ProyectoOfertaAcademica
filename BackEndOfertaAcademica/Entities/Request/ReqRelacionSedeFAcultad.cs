9e3402dc8d8044316248460bb3ffc61a373fb70f
aerm <andres.rojas16@ulatina.net>
2023-07-26 21:32:30 -0600

    Creacion de las entidades relacion Sede Facultad

---
+++ BackEndOfertaAcademica/Entities/Request/ReqRelacionSedeFAcultad.cs
@@ -0,0 +1,14 @@
+﻿using BackEndOfertaAcademica.Entities.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackEndOfertaAcademica.Entities
+{
+    public class ReqRelacionSedeFAcultad : ReqBase
+    {
+        public RelacionSedeFacultad RelacionSedeFacultad { get; set;}
+    }
+}
---
+++ BackEndOfertaAcademica/Logic/LogicaRelacionSedeFacultad.cs
@@ -0,0 +1,42 @@
+﻿using BackEndOfertaAcademica.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackEndOfertaAcademica.Logic
+{
+    internal class LogicaRelacionSedeFacultad
+    {
+
+        public ResRelacionSedeFacultad nuevaRelacionSedeFacultad (ReqRelacionSedeFAcultad request)
+        {
+            ResRelacionSedeFacultad response = new ResRelacionSedeFacultad ();
+            response.errorList = new List<string> ();
+
+            try
+            {
+                if (request == null)
+                {
+                    response.result = false;
+                    response.errorList.Add ("El request de RelacionSedeFacultad es nulo");
+                }
+                else
+                {
+                    if (string.IsNullOrEmpty(request.RelacionSedeFacultad.idFacultad.ToString()))
+                    {
+                        response.result = false;
+                        response.errorList = 
+                    }
+                }
+            }catch (Exception e)
+            {
+
+            }
+
+            return response;
+        }
+
+    }
+}
