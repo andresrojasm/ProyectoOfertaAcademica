04777aeb75e68ecd22eb4e43206d34d45bb9fddb
WilliLaptop <william_herrera_araya@outlook.com>
2023-08-20 15:20:26 -0600

    Fron los Req y Res

---
+++ From/Entities/Carrera.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace From.Entities
+{
+    public class Carrera
+    {
+        public string idCarrera { get; set; }
+        public string nombre { get; set; }
+        public int idFacultad { get; set; }
+        public int idGrado { get; set; }
+    }
+}
