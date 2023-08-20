266d9e26ede1213581e08dba69d0ac28d5dc2262
aerm <andres.rojas16@ulatina.net>
2023-08-20 13:47:29 -0600

    Creacion Factory
    SWe creo la clase utilitaria Factory para implementar la instanciacion de usuarios segun se requiera

---
+++ BackEndOfertaAcademica/Utilities/Factory.cs
@@ -0,0 +1,47 @@
+﻿using BackEndOfertaAcademica.DataAccess;
+using BackEndOfertaAcademica.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace BackEndOfertaAcademica.Utilities
+{
+    public class Factory
+    {
+        public static Usuario factoryUsuario(GET_USUARIOResult rs)
+        {
+            return createUser(rs.CEDULA, rs.NOMBRE, rs.NOMBRE, rs.EDAD, rs.CORREO, rs.CODIGODOCENTE, 
+                rs.FECHA, rs.ID_ROLLES, rs.ACTIVO) ;
+        }
+
+        public static List<Usuario> factoryListaUsuario(List<GET_LISTA_USUARIOSResult> rs)
+        {
+            List<Usuario> listaUsuarios = new List<Usuario>();
+
+            foreach (GET_LISTA_USUARIOSResult usuario in rs)
+            {
+                listaUsuarios.Add(createUser(usuario.CEDULA, usuario.NOMBRE, usuario.NOMBRE, 
+                    usuario.EDAD, usuario.CORREO, usuario.CODIGODOCENTE, usuario.FECHA, 
+                    usuario.ID_ROLLES, usuario.ACTIVO));
+            }
+
+            return listaUsuarios;
+        }
+
+        private static Usuario createUser(int cedula, string nombre, string apellidos, int edad,
+            string correo, string codigoDocente, DateTime fechaCreacion, int rol, bool activo)
+        {
+            Usuario usuario = new Usuario();
+            usuario.cedula = cedula;
+            usuario.nombre = nombre;
+            usuario.apellidos = apellidos;
+            usuario.edad = edad;
+            usuario.correo = correo;
+            usuario.codigoDocente = codigoDocente;
+            usuario.fechaCreacion = fechaCreacion;
+            usuario.rol =rol;
+            usuario.activo = activo;
+
+            return usuario;
+        }
+    }
+}
