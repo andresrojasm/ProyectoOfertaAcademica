0819b0d61446ac640346df51ec6c72d56b210619
WilliLaptop <william_herrera_araya@outlook.com>
2023-08-20 19:30:42 -0600

    Fron Login y pages view, correcciones de problemas.

--- From/App.xaml.cs
+++ From/App.xaml.cs
@@ -1,11 +1,24 @@
-﻿namespace From;
+﻿using From.Handlers;
+using Microsoft.Maui.Platform;
+
+namespace From;
 
 public partial class App : Application
 {
 	public App()
 	{
 		InitializeComponent();
-
-		MainPage = new AppShell();
-	}
+        Microsoft.Maui.Handlers.EntryHandler.Mapper.AppendToMapping(nameof(BorderlessEntry), (handler, view) =>
+        {
+            if (view is BorderlessEntry)
+            {
+#if __ANDROID__
+                handler.PlatformView.SetBackgroundColor(Colors.Transparent.ToPlatform());
+#elif __IOS__
+                handler.PlatformView.BorderStyle = UIKit.UITextBorderStyle.None;
+#endif
+            }
+        });
+        MainPage = new AppShell();
+    }
 }
