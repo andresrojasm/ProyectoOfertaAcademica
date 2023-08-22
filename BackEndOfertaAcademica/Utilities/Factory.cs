5c3dd7abfe155dc4ad445d10e586cae4dd9b0572
aerm <andres.rojas16@ulatina.net>
2023-08-22 00:01:50 -0600

    Hotfix Usuario
    El nombre es el apellido?

--- BackEndOfertaAcademica/Utilities/Factory.cs
+++ BackEndOfertaAcademica/Utilities/Factory.cs
@@ -11,6 +11,7 @@ namespace BackEndOfertaAcademica.Utilities
         {
             return createUser(login.CEDULA, login.NOMBRE, login.APELLIDOS, 0, "", "", DateTime.Now, login.ID_ROLLES, true);
         }
+
         public static Usuario factoryUsuario(GET_USUARIOResult rs)
         {
             return createUser(rs.CEDULA, rs.NOMBRE, rs.NOMBRE, rs.EDAD, rs.CORREO, rs.CODIGODOCENTE, 
@@ -23,7 +24,7 @@ namespace BackEndOfertaAcademica.Utilities
 
             foreach (GET_LISTA_USUARIOSResult usuario in rs)
             {
-                listaUsuarios.Add(createUser(usuario.CEDULA, usuario.NOMBRE, usuario.NOMBRE, 
+                listaUsuarios.Add(createUser(usuario.CEDULA, usuario.NOMBRE, usuario.APELLIDOS, 
                     usuario.EDAD, usuario.CORREO, usuario.CODIGODOCENTE, usuario.FECHA, 
                     usuario.ID_ROLLES, usuario.ACTIVO));
             }
@@ -46,6 +47,25 @@ namespace BackEndOfertaAcademica.Utilities
             usuario.activo = activo;
 
             return usuario;
+        }
+
+        public static List<Curso> fatoryListaCursos(List<GET_LISTA_CURSOSResult> rs)
+        {
+            Curso curso = new Curso();
+            List<Curso> listaCursos = new List<Curso>();
+
+            foreach (GET_LISTA_CURSOSResult cursos in rs)
+            {
+                
+                curso.idCurso = cursos.ID_CURSO;
+                cursos.NOMBRECURSO = cursos.NOMBRECURSO;
+                curso.credito = cursos.CREDITO;
+                curso.bloque = cursos.BLOQUE;
+
+                listaCursos.Add(curso);
+            }
+
+            return listaCursos;
         }
     }
 }
